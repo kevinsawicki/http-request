453118b2fd6782898abd259fb86796f0042022f1
Kevin Sawicki <kevinsawicki@gmail.com>
2012-04-22 19:34:08 -0700

    Add unit test that posts multiple files to URL

--- lib/src/test/java/com/github/kevinsawicki/http/HttpRequestTest.java
+++ lib/src/test/java/com/github/kevinsawicki/http/HttpRequestTest.java
@@ -550,6 +550,33 @@ public class HttpRequestTest extends ServerTestCase {
 	}
 
 	/**
+	 * Make a POST request with multiple files in the body
+	 *
+	 * @throws Exception
+	 */
+	@Test
+	public void postMultipleFiles() throws Exception {
+		final AtomicReference<String> body = new AtomicReference<String>();
+		String url = setUp(new RequestHandler() {
+
+			public void handle(Request request, HttpServletResponse response) {
+				body.set(new String(read()));
+				response.setStatus(HTTP_OK);
+			}
+		});
+
+		File file1 = File.createTempFile("post", ".txt");
+		new FileWriter(file1).append("hello").close();
+
+		File file2 = File.createTempFile("post", ".txt");
+		new FileWriter(file2).append(" world").close();
+
+		int code = post(url).send(file1).send(file2).code();
+		assertEquals(HTTP_OK, code);
+		assertEquals("hello world", body.get());
+	}
+
+	/**
 	 * Make a POST request with a non-empty request body
 	 *
 	 * @throws Exception
