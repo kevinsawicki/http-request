7c74f39ace47c1cdec12fcaaa44ff638b18ee58d
Kevin Sawicki <kevinsawicki@gmail.com>
2012-06-11 09:17:01 -0700

    Add unit test of posting form entry with null value

--- lib/src/test/java/com/github/kevinsawicki/http/HttpRequestTest.java
+++ lib/src/test/java/com/github/kevinsawicki/http/HttpRequestTest.java
@@ -1866,4 +1866,29 @@ public class HttpRequestTest extends ServerTestCase {
 		assertEquals(HTTP_OK, code);
 		assertEquals("name=user&number=100", body.get());
 	}
+
+	/**
+	 * Verify sending form data where entry value is null
+	 *
+	 * @throws Exception
+	 */
+	@Test
+	public void postFormEntryWithNullValue() throws Exception {
+		final AtomicReference<String> body = new AtomicReference<String>();
+		String url = setUp(new RequestHandler() {
+
+			public void handle(Request request, HttpServletResponse response) {
+				body.set(new String(read()));
+				response.setStatus(HTTP_OK);
+			}
+		});
+		Map<String, String> data = new LinkedHashMap<String, String>();
+		data.put("name", null);
+		HttpRequest request = post(url);
+		for (Entry<String, String> entry : data.entrySet())
+			request.form(entry);
+		int code = request.code();
+		assertEquals(HTTP_OK, code);
+		assertEquals("name=", body.get());
+	}
 }
