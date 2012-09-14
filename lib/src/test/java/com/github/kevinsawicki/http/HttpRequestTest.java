8cae821bf2122f5cbffb6ea5d83ce5d1613bbde9
Kevin Sawicki <kevinsawicki@gmail.com>
2012-09-14 10:18:32 -0700

    Add test of reused hostname verifier

--- lib/src/test/java/com/github/kevinsawicki/http/HttpRequestTest.java
+++ lib/src/test/java/com/github/kevinsawicki/http/HttpRequestTest.java
@@ -1477,6 +1477,22 @@ public class HttpRequestTest extends ServerTestCase {
   }
 
   /**
+   * Verify single hostname verifier is created across all calls
+   */
+  @Test
+  public void singleVerifier() {
+    HttpRequest request1 = get("https://localhost").trustAllHosts();
+    HttpRequest request2 = get("https://localhost").trustAllHosts();
+    assertNotNull(((HttpsURLConnection) request1.getConnection())
+        .getHostnameVerifier());
+    assertNotNull(((HttpsURLConnection) request2.getConnection())
+        .getHostnameVerifier());
+    assertEquals(
+        ((HttpsURLConnection) request1.getConnection()).getHostnameVerifier(),
+        ((HttpsURLConnection) request2.getConnection()).getHostnameVerifier());
+  }
+
+  /**
    * Send a stream that throws an exception when read from
    *
    * @throws Exception
