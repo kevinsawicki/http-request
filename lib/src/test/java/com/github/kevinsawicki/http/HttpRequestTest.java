dfd1e5073b4325d52a4f7fe30d4797b8f039286d
Kevin Sawicki <kevinsawicki@gmail.com>
2012-09-17 09:55:14 -0700

    Add test of post with escaped varargs query params

--- lib/src/test/java/com/github/kevinsawicki/http/HttpRequestTest.java
+++ lib/src/test/java/com/github/kevinsawicki/http/HttpRequestTest.java
@@ -2109,6 +2109,31 @@ public class HttpRequestTest extends ServerTestCase {
   }
 
   /**
+   * Verify POST with escaped query parameters
+   *
+   * @throws Exception
+   */
+  @Test
+  public void postWithEscapedVarargsQueryParams() throws Exception {
+    final Map<String, String> outputParams = new HashMap<String, String>();
+    final AtomicReference<String> method = new AtomicReference<String>();
+    String url = setUp(new RequestHandler() {
+
+      public void handle(Request request, HttpServletResponse response) {
+        method.set(request.getMethod());
+        outputParams.put("name", request.getParameter("name"));
+        outputParams.put("number", request.getParameter("number"));
+        response.setStatus(HTTP_OK);
+      }
+    });
+    HttpRequest request = post(url, true, "name", "us er", "number", "100");
+    assertTrue(request.ok());
+    assertEquals("POST", method.get());
+    assertEquals("us er", outputParams.get("name"));
+    assertEquals("100", outputParams.get("number"));
+  }
+
+  /**
    * Verify POST with numeric query parameters
    *
    * @throws Exception
