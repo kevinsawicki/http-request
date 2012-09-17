7d7c319668b2abd782df5dd11694c7e006de4608
Kevin Sawicki <kevinsawicki@gmail.com>
2012-09-17 10:35:56 -0700

    Add test of no server compression when requested

--- lib/src/test/java/com/github/kevinsawicki/http/HttpRequestTest.java
+++ lib/src/test/java/com/github/kevinsawicki/http/HttpRequestTest.java
@@ -1644,6 +1644,28 @@ public class HttpRequestTest extends ServerTestCase {
   }
 
   /**
+   * Make a GET request that should be compressed but isn't
+   *
+   * @throws Exception
+   */
+  @Test
+  public void getNonGzippedWithUncompressEnabled() throws Exception {
+    String url = setUp(new RequestHandler() {
+
+      public void handle(Request request, HttpServletResponse response) {
+        response.setStatus(HTTP_OK);
+        if (!"gzip".equals(request.getHeader("Accept-Encoding")))
+          return;
+
+        write("hello not compressed");
+      }
+    });
+    HttpRequest request = get(url).acceptGzipEncoding().uncompress(true);
+    assertTrue(request.ok());
+    assertEquals("hello not compressed", request.body(CHARSET_UTF8));
+  }
+
+  /**
    * Get header with multiple response values
    *
    * @throws Exception
