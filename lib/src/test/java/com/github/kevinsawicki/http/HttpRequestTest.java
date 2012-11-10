6dbdc1a613fe55031d424ba94abeb19be7b562d3
Kevin Sawicki <kevinsawicki@gmail.com>
2012-11-10 17:39:22 +0100

    Add assert for setting null Number header

--- lib/src/test/java/com/github/kevinsawicki/http/HttpRequestTest.java
+++ lib/src/test/java/com/github/kevinsawicki/http/HttpRequestTest.java
@@ -1266,18 +1266,21 @@ public class HttpRequestTest extends ServerTestCase {
    * @throws Exception
    */
   @Test
-  public void numberHead() throws Exception {
+  public void numberHeader() throws Exception {
     final AtomicReference<String> h1 = new AtomicReference<String>();
+    final AtomicReference<String> h2 = new AtomicReference<String>();
     handler = new RequestHandler() {
 
       @Override
       public void handle(Request request, HttpServletResponse response) {
         response.setStatus(HTTP_OK);
         h1.set(request.getHeader("h1"));
+        h2.set(request.getHeader("h2"));
       }
     };
-    assertTrue(get(url).header("h1", 5).ok());
+    assertTrue(get(url).header("h1", 5).header("h2", (Number) null).ok());
     assertEquals("5", h1.get());
+    assertEquals("", h2.get());
   }
 
   /**
