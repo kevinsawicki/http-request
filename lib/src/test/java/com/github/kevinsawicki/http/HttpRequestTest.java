7ade695f01616b2b02f3cfb39d86e3b168624d9c
Kevin Sawicki <kevinsawicki@gmail.com>
2012-12-09 15:48:16 -0800

    Add unit tests of setting Content-Type request header

--- lib/src/test/java/com/github/kevinsawicki/http/HttpRequestTest.java
+++ lib/src/test/java/com/github/kevinsawicki/http/HttpRequestTest.java
@@ -1208,6 +1208,66 @@ public class HttpRequestTest extends ServerTestCase {
   }
 
   /**
+   * Verify 'Content-Type' header
+   *
+   * @throws Exception
+   */
+  @Test
+  public void requestContentType() throws Exception {
+    final AtomicReference<String> contentType = new AtomicReference<String>();
+    handler = new RequestHandler() {
+
+      @Override
+      public void handle(Request request, HttpServletResponse response) {
+        contentType.set(request.getContentType());
+        response.setStatus(HTTP_OK);
+      }
+    };
+    assertTrue(post(url).contentType("text/html", "UTF-8").ok());
+    assertEquals("text/html; charset=UTF-8", contentType.get());
+  }
+
+  /**
+   * Verify 'Content-Type' header
+   *
+   * @throws Exception
+   */
+  @Test
+  public void requestContentTypeNullCharset() throws Exception {
+    final AtomicReference<String> contentType = new AtomicReference<String>();
+    handler = new RequestHandler() {
+
+      @Override
+      public void handle(Request request, HttpServletResponse response) {
+        contentType.set(request.getContentType());
+        response.setStatus(HTTP_OK);
+      }
+    };
+    assertTrue(post(url).contentType("text/html", null).ok());
+    assertEquals("text/html", contentType.get());
+  }
+
+  /**
+   * Verify 'Content-Type' header
+   *
+   * @throws Exception
+   */
+  @Test
+  public void requestContentTypeEmptyCharset() throws Exception {
+    final AtomicReference<String> contentType = new AtomicReference<String>();
+    handler = new RequestHandler() {
+
+      @Override
+      public void handle(Request request, HttpServletResponse response) {
+        contentType.set(request.getContentType());
+        response.setStatus(HTTP_OK);
+      }
+    };
+    assertTrue(post(url).contentType("text/html", "").ok());
+    assertEquals("text/html", contentType.get());
+  }
+
+  /**
    * Verify 'Cache-Control' header
    *
    * @throws Exception
