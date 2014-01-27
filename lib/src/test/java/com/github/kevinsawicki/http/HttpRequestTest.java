1a3581a8cd1de42179b50d1397fd8b1d8fcb2533
Kevin Sawicki <kevinsawicki@gmail.com>
2014-01-27 13:57:15 -0800

    Bump copyright to 2014

--- lib/src/test/java/com/github/kevinsawicki/http/HttpRequestTest.java
+++ lib/src/test/java/com/github/kevinsawicki/http/HttpRequestTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2011 Kevin Sawicki <kevinsawicki@gmail.com>
+ * Copyright (c) 2014 Kevin Sawicki <kevinsawicki@gmail.com>
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to
