e857710004996044b2a891d1f5bf9f250a2bb40b
Kevin Sawicki <kevinsawicki@gmail.com>
2015-01-21 11:35:40 -0800

    Use private static method instead of class

--- lib/src/main/java/com/github/kevinsawicki/http/HttpRequest.java
+++ lib/src/main/java/com/github/kevinsawicki/http/HttpRequest.java
@@ -338,7 +338,7 @@ public class HttpRequest {
       final StringBuilder result) {
     Object value = valueObj;
     if (value != null && value.getClass().isArray())
-      value = ArrayUtils.arrayToList(value);
+      value = arrayToList(value);
     if (value instanceof Iterable<?>) {
       Iterator<?> iterator = ((Iterable<?>) value).iterator();
       while (true) {
@@ -828,36 +828,34 @@ public class HttpRequest {
     }
   }
 
-  private static class ArrayUtils {
+  /**
+   * Represents array of any type as list of objects so we can easily iterate over it
+   * @param array of elements
+   * @return list with the same elements
+   */
+  private static List<Object> arrayToList(final Object array) {
+    if (array instanceof Object[])
+      return Arrays.asList((Object[]) array);
 
-    /**
-     * Represents array of any type as list of objects so we can easily iterate over it
-     * @param array of elements
-     * @return list with the same elements
-     */
-    public static List<Object> arrayToList(Object array) {
-      List<Object> result = new ArrayList<Object>();
-      if (array instanceof Object[])
-        result = Arrays.asList((Object[]) array);
-      // Arrays of the primitive types can't be cast to array of Object, so this:
-      else if (array instanceof int[])
-        for (int value : (int[]) array) result.add(value);
-      else if (array instanceof boolean[])
-        for (boolean value : (boolean[]) array) result.add(value);
-      else if (array instanceof long[])
-        for (long value : (long[]) array) result.add(value);
-      else if (array instanceof float[])
-        for (float value : (float[]) array) result.add(value);
-      else if (array instanceof double[])
-        for (double value : (double[]) array) result.add(value);
-      else if (array instanceof short[])
-        for (short value : (short[]) array) result.add(value);
-      else if (array instanceof byte[])
-        for (byte value : (byte[]) array) result.add(value);
-      else if (array instanceof char[])
-        for (char value : (char[]) array) result.add(value);
-      return result;
-    }
+    List<Object> result = new ArrayList<Object>();
+    // Arrays of the primitive types can't be cast to array of Object, so this:
+    if (array instanceof int[])
+      for (int value : (int[]) array) result.add(value);
+    else if (array instanceof boolean[])
+      for (boolean value : (boolean[]) array) result.add(value);
+    else if (array instanceof long[])
+      for (long value : (long[]) array) result.add(value);
+    else if (array instanceof float[])
+      for (float value : (float[]) array) result.add(value);
+    else if (array instanceof double[])
+      for (double value : (double[]) array) result.add(value);
+    else if (array instanceof short[])
+      for (short value : (short[]) array) result.add(value);
+    else if (array instanceof byte[])
+      for (byte value : (byte[]) array) result.add(value);
+    else if (array instanceof char[])
+      for (char value : (char[]) array) result.add(value);
+    return result;
   }
 
   /**
